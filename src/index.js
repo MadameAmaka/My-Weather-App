5a6f4907cff3f0112a366d3e2b44cda03f9dee88
MadameAmaka <amonwuli@gmail.com>
2024-08-26 18:53:21 +0100

    added weather icon to js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ function updateWeather(response) {
   let windElement = document.querySelector("#wind-condition");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+let iconElement = document.querySelector("#weather-icon");
 
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
